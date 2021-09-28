02f040efe43d2bf57e36352370a99e18486b8825
Billou <32905881+billouetaudrey@users.noreply.github.com>
2021-09-28 13:47:00 +0200

    Update snips.py

--- userbot/modules/snips.py
+++ userbot/modules/snips.py
@@ -98,7 +98,7 @@ async def on_snip_list(event):
     await event.edit(message)
 
 
-@register(outgoing=True, pattern=r"^\.remsnip (\w*)")
+@register(outgoing=True, pattern=r"^\.snipd (\w*)")
 async def on_snip_delete(event):
     """For .remsnip command, deletes a snip."""
     try:
